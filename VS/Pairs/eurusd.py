36eec6f3039a97ca049b71042841020681d45a4b
Yugant Shekhar <yugantshekhar@gmail.com>
2023-10-01 19:08:33 +0530

    Live Code Testing

---
+++ VS/Pairs/eurusd.py
@@ -0,0 +1,6 @@
+from Strategy import LiveTrade
+
+eurusd = LiveTrade(['EURUSD', 10, 24000])
+
+eurusd.Caller('EURUSD')
+
---
+++ VS/Pairs/usdjpy.py
@@ -0,0 +1,6 @@
+from Strategy import LiveTrade
+
+usdjpy = LiveTrade(['USDJPY', 6.69582, 24000])
+
+usdjpy.Caller('USDJPY')
+
